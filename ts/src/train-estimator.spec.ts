3b7351dc3deededdc18cb078eadd542ddcefcf3f
alexboisseau <alex.boisseau79@gmail.com>
2023-05-05 12:06:41 +0200

    refactor: refactor exception cases tests

--- ts/src/train-estimator.spec.ts
+++ ts/src/train-estimator.spec.ts
@@ -1,6 +1,5 @@
 import { TrainTicketEstimator } from './train-estimator';
 import {
-  DiscountCard,
   InvalidTripInputException,
   Passenger,
   TripDetails,
@@ -14,28 +13,24 @@ class TrainTicketEstimatorOverload extends TrainTicketEstimator {
 }
 
 describe('train estimator', function () {
-  it('should return 0 when no passenger', async function () {
-    const tripDetails = new TripDetails('Paris', 'Milan', new Date());
-    const tripRequest = new TripRequest(tripDetails, []);
-    const trainTicketEstimator = new TrainTicketEstimatorOverload();
-    const result = await trainTicketEstimator.estimate(tripRequest);
-    expect(result).toBe(0);
+  let alice: Passenger;
+  let trainTicketEstimator: TrainTicketEstimatorOverload;
+
+  beforeAll(() => {
+    alice = new Passenger(20, []);
+    trainTicketEstimator = new TrainTicketEstimatorOverload();
   });
-});
-describe('exceptions handling', function () {
-  const fakePassenger = new Passenger(20, []);
+
   it('should return an exception when start city is invalid', async function () {
     const tripDetails = new TripDetails('', 'Marseille', new Date());
-    const tripRequest = new TripRequest(tripDetails, [fakePassenger]);
-    const trainTicketEstimator = new TrainTicketEstimatorOverload();
+    const tripRequest = new TripRequest(tripDetails, [alice]);
     await expect(async () => await trainTicketEstimator.estimate(tripRequest)).rejects.toEqual(
       new InvalidTripInputException('Start city is invalid')
     );
   });
   it('should return an exception when destination city is invalid', async function () {
     const tripDetails = new TripDetails('Paris', '', new Date());
-    const tripRequest = new TripRequest(tripDetails, [fakePassenger]);
-    const trainTicketEstimator = new TrainTicketEstimatorOverload();
+    const tripRequest = new TripRequest(tripDetails, [alice]);
     await expect(async () => await trainTicketEstimator.estimate(tripRequest)).rejects.toEqual(
       new InvalidTripInputException('Destination city is invalid')
     );
@@ -44,8 +39,7 @@ describe('exceptions handling', function () {
     const badDate = new Date();
     badDate.setDate(badDate.getDay() - 10);
     const tripDetails = new TripDetails('Paris', 'Marseille', badDate);
-    const tripRequest = new TripRequest(tripDetails, [fakePassenger]);
-    const trainTicketEstimator = new TrainTicketEstimatorOverload();
+    const tripRequest = new TripRequest(tripDetails, [alice]);
     await expect(async () => await trainTicketEstimator.estimate(tripRequest)).rejects.toEqual(
       new InvalidTripInputException('Date is invalid')
     );
@@ -54,9 +48,15 @@ describe('exceptions handling', function () {
     const fakePassengerWithBadAge = new Passenger(-1, []);
     const tripDetails = new TripDetails('Paris', 'Marseille', new Date());
     const tripRequest = new TripRequest(tripDetails, [fakePassengerWithBadAge]);
-    const trainTicketEstimator = new TrainTicketEstimatorOverload();
     await expect(async () => await trainTicketEstimator.estimate(tripRequest)).rejects.toEqual(
       new InvalidTripInputException('Age is invalid')
     );
+  });
+
+  it('should return 0 when no passenger', async function () {
+    const tripDetails = new TripDetails('Paris', 'Milan', new Date());
+    const tripRequest = new TripRequest(tripDetails, []);
+    const result = await trainTicketEstimator.estimate(tripRequest);
+    expect(result).toBe(0);
   });
 });
