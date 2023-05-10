176dd86390dfd56ecd0e6850900fabfbec814598
Alex Boisseau <46025892+alexboisseau@users.noreply.github.com>
2023-05-10 14:51:02 +0200

    Unit tests (#1)
    * fix: use a try catch for the call to the sncf api to catch an error during the parsing operation
    
    * feat: create a method to get price from api in train estimator class and overload this one for test
    
    * tests: add exceptions cases
    
    * refactor: add a method to validate tripRequest input passed to the estimate method
    
    * refactor: refactor exception cases tests
    
    * add: test for less than 1 year old passenger
    
    * fix: add a continue statement to fix case of a passenger is less thant 1 years old$
    
    * refactor: remove useless variables
    
    * test: write test for exceptions, static prices and dynamic prices
    
    ---------
    
    Co-authored-by: Alexandre Kersulec <kersulec.kersulec@gmail.com>
    Co-authored-by: tombiato <biatotom@gmail.com>

--- ts/src/train-estimator.ts
+++ ts/src/train-estimator.ts
@@ -6,75 +6,81 @@ import {
 } from './model/trip.request';
 
 export class TrainTicketEstimator {
-  async estimate(trainDetails: TripRequest): Promise<number> {
-    if (trainDetails.passengers.length === 0) {
+  protected async getPriceFromApi(from: string, to: string, when: Date): Promise<number> {
+    throw new Error('Should not be call from a test');
+  }
+
+  validTripRequestInput(tripRequest: TripRequest) {
+    if (tripRequest.passengers.length === 0) {
       return 0;
     }
 
-    if (trainDetails.details.from.trim().length === 0) {
+    if (tripRequest.details.from.trim().length === 0) {
       throw new InvalidTripInputException('Start city is invalid');
     }
 
-    if (trainDetails.details.to.trim().length === 0) {
+    if (tripRequest.details.to.trim().length === 0) {
       throw new InvalidTripInputException('Destination city is invalid');
     }
 
     if (
-      trainDetails.details.when <
-      new Date(new Date().getFullYear(), new Date().getMonth(), new Date().getDay(), 0, 0, 0)
+      tripRequest.details.when <
+      new Date(new Date().getFullYear(), new Date().getMonth(), new Date().getDate(), 0, 0, 0)
     ) {
       throw new InvalidTripInputException('Date is invalid');
     }
 
-    // TODO USE THIS LINE AT THE END
-    const b =
-      (
-        await (
-          await fetch(
-            `https://sncf.com/api/train/estimate/price?from=${trainDetails.details.from}&to=${trainDetails.details.to}&date=${trainDetails.details.when}`
-          )
-        ).json()
-      )?.price || -1;
+    tripRequest.passengers.forEach((passenger) => {
+      if (passenger.age < 0) {
+        throw new InvalidTripInputException('Age is invalid');
+      }
+    });
+  }
 
-    if (b === -1) {
+  async estimate(tripRequest: TripRequest): Promise<number> {
+    this.validTripRequestInput(tripRequest);
+
+    let basePrice;
+    try {
+      basePrice = await this.getPriceFromApi(
+        tripRequest.details.from,
+        tripRequest.details.to,
+        tripRequest.details.when
+      );
+    } catch (error) {
       throw new ApiException();
     }
 
-    const passengers = trainDetails.passengers;
-    let tot = 0;
-    let tmp = b;
+    const passengers = tripRequest.passengers;
+    let totalPrice = 0;
+    let tmp = basePrice;
     for (let i = 0; i < passengers.length; i++) {
-      if (passengers[i].age < 0) {
-        throw new InvalidTripInputException('Age is invalid');
-      }
       if (passengers[i].age < 1) {
         continue;
-      }
-      // Seniors
-      else if (passengers[i].age <= 17) {
-        tmp = b * 0.6;
+      } else if (passengers[i].age <= 17) {
+        tmp = basePrice * 0.6;
       } else if (passengers[i].age >= 70) {
-        tmp = b * 0.8;
+        tmp = basePrice * 0.8;
         if (passengers[i].discounts.includes(DiscountCard.Senior)) {
-          tmp -= b * 0.2;
+          tmp -= basePrice * 0.2;
         }
       } else {
-        tmp = b * 1.2;
+        tmp = basePrice * 1.2;
       }
 
       const d = new Date();
-      if (trainDetails.details.when.getTime() >= d.setDate(d.getDate() + 30)) {
-        tmp -= b * 0.2;
-      } else if (trainDetails.details.when.getTime() > d.setDate(d.getDate() - 30 + 5)) {
-        const date1 = trainDetails.details.when;
+      if (tripRequest.details.when.getTime() >= d.setDate(d.getDate() + 30)) {
+        tmp -= basePrice * 0.2;
+      } else if (tripRequest.details.when.getTime() > d.setDate(d.getDate() - 30 + 5)) {
+        const date1 = tripRequest.details.when;
         const date2 = new Date();
         //https://stackoverflow.com/questions/43735678/typescript-get-difference-between-two-dates-in-days
         const diff = Math.abs(date1.getTime() - date2.getTime());
         const diffDays = Math.ceil(diff / (1000 * 3600 * 24));
 
-        tmp += (20 - diffDays) * 0.02 * b; // I tried. it works. I don't know why.
+        tmp += (20 - diffDays) * 0.02 * basePrice; // I tried. it works. I don't know why.
       } else {
-        tmp += b;
+        tmp += basePrice;
       }
 
       if (passengers[i].age > 0 && passengers[i].age < 4) {
@@ -85,42 +91,42 @@ export class TrainTicketEstimator {
         tmp = 1;
       }
 
-      tot += tmp;
-      tmp = b;
+      totalPrice += tmp;
+      tmp = basePrice;
     }
 
     if (passengers.length == 2) {
-      let cp = false;
-      let mn = false;
+      let isCouple = false;
+      let isHalfCouple = false;
       for (let i = 0; i < passengers.length; i++) {
         if (passengers[i].discounts.includes(DiscountCard.Couple)) {
-          cp = true;
+          isCouple = true;
         }
         if (passengers[i].age < 18) {
-          mn = true;
+          isHalfCouple = true;
         }
       }
-      if (cp && !mn) {
-        tot -= b * 0.2 * 2;
+      if (isCouple && !isHalfCouple) {
+        totalPrice -= basePrice * 0.2 * 2;
       }
     }
 
     if (passengers.length == 1) {
-      let cp = false;
-      let mn = false;
+      let isCouple = false;
+      let isHalfCouple = false;
       for (let i = 0; i < passengers.length; i++) {
         if (passengers[i].discounts.includes(DiscountCard.HalfCouple)) {
-          cp = true;
+          isCouple = true;
         }
         if (passengers[i].age < 18) {
-          mn = true;
+          isHalfCouple = true;
         }
       }
-      if (cp && !mn) {
-        tot -= b * 0.1;
+      if (isCouple && !isHalfCouple) {
+        totalPrice -= basePrice * 0.1;
       }
     }
 
-    return tot;
+    return totalPrice;
   }
 }
